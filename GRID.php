d9418cd892566a4f270b04201eb7126be2d03e9d
mcord-oss <ordonjulius1@gmail.com>
2025-05-11 10:22:58 +0800

    Add files via upload

---
+++ GRID.php
@@ -0,0 +1,42 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./GRID-STYLE.css">
+</head>
+<body>
+    <!-- <h1>GRID</h1>
+    <div class="main-container">
+        <div class="main-content">
+            <h1>main containt</h1>
+            <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Blanditiis recusandae corrupti itaque alias, illum earum officia perspiciatis delectus illo hic dolorum fugit natus nisi totam minima sunt, est quos amet dicta. Sequi, rem iure itaque fuga totam repudiandae optio excepturi aspernatur blanditiis dicta. Nihil, deserunt aperiam reprehenderit esse ducimus ipsam!</p>
+            <p>Lorem ipsum, dolor sit amet consectetur adipisicing elit. Quas rem vel eum quae rerum quibusdam exercitationem quo! Optio eaque impedit iusto id omnis eum amet dolores quia, modi saepe suscipit.</p>
+            <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Odio enim fugiat facilis quam illum vitae dolorem cupiditate quidem, doloribus magni! Quod quasi delectus porro distinctio veritatis tempore recusandae ducimus sed.</p>
+        </div>
+        <div class="left-sidebar">
+            <h2>left sidebar</h2>
+            <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Magnam deleniti sed aperiam minus. Excepturi repellat sint expedita! Sapiente quae vero sunt dicta distinctio delectus consequuntur ipsam reprehenderit, labore voluptatem assumenda.</p>
+        </div>
+        <div class="rigth-sidebar">
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa nisi nulla architecto laboriosam quia. Impedit soluta quae inventore facere ab! Qui deserunt reiciendis voluptate, velit non, eos, repellendus optio dolorum dicta doloremque dolore amet similique accusantium animi culpa sint voluptatibus rerum quibusdam ex architecto suscipit! Optio possimus fugiat accusantium sit nobis commodi unde alias reprehenderit minus, incidunt dolorum? Odio ullam sit ad totam non voluptate, harum debitis tempore voluptatibus doloremque, fugit nesciunt. Magni aliquid ratione nostrum officia omnis tenetur, dicta eius vel, consectetur officiis quod maiores? Maiores eos dicta, assumenda animi nihil architecto odio deleniti voluptatem placeat vero rerum minima?</p>
+        </div> -->
+
+        <!-- <div class="box-container">
+            <div class="box-1">Box 1</div>
+            <div class="box-2">Box 2</div>
+            <div class="box-3">Box 3</div>
+        </div>
+    </div> -->
+
+    <div class="gallary">
+        <img src="images/brown.jfif" alt="">
+        <img src="images/celine.jfif" alt="">
+        <img src="images/eli.jfif" alt="">
+        <img src="images/img5.jfif" alt="">
+        <img src="images/manny.jfif" alt="">
+        <img src="images/img3.jfif" alt="">
+    </div>
+</body>
+</html>
